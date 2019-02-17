be7f01ccc6993b91906fbc20bc3d4561a18d6f42
PRASHANTHI <PRASHANTHI@LAPTOP-2NA4FFI8>
2019-02-17 23:46:56 +0530

    First

---
+++ Selenium/src/main/java/PrashanthiSelenium/Selenium/App.java
@@ -0,0 +1,13 @@
+package PrashanthiSelenium.Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
